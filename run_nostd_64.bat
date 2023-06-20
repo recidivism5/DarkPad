915b4a881c3ceb4adb2d64ccfb6490694c36d79f
Ian Bryant <ijb2020syr@gmail.com>
2023-06-20 13:04:29 -0400

    /Gz (use stdcall unless told otherwise)

--- run_nostd_64.bat
+++ run_nostd_64.bat
@@ -3,6 +3,6 @@ if not defined DevEnvDir (
     call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
 )
 rc res.rc
-cl /nologo /O1 /w /GS- darkpad.c /Fe"darkpad.exe" /link /nodefaultlib /subsystem:windows kernel32.lib shell32.lib gdi32.lib user32.lib uxtheme.lib ole32.lib uuid.lib res.res
+cl /nologo /O1 /w /Gz /GS- darkpad.c /Fe"darkpad.exe" /link /nodefaultlib /subsystem:windows kernel32.lib shell32.lib gdi32.lib user32.lib uxtheme.lib ole32.lib uuid.lib res.res
 rm darkpad.obj
 darkpad
