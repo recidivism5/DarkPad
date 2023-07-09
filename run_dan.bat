b1c1b611cc975afeab7f9d9f34027363602c22db
fiumad <technodan143@gmail.com>
2023-07-09 16:44:32 -0400

    added run_dan.bat

---
+++ run_dan.bat
@@ -0,0 +1,10 @@
+@echo off
+if not defined DevEnvDir (
+    call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
+)
+del darkpad.exe
+rc res.rc
+cl /nologo /O1 /w /Gz /GS- darkpad.c /Fe"darkpad.exe" /link /nodefaultlib /subsystem:windows kernel32.lib shell32.lib gdi32.lib user32.lib ole32.lib uuid.lib dwmapi.lib comdlg32.lib res.res
+mt.exe -manifest darkpad.exe.manifest -outputresource:darkpad.exe;1
+del darkpad.obj
+darkpad.exe
