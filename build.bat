7b01ef9889f75f6c85f2b29397b0dda06214662d
Ian Bryant <ijb2020syr@gmail.com>
2023-07-19 21:28:07 -0400

    removed dependency on tcc

--- build.bat
+++ build.bat
@@ -2,7 +2,10 @@
 if not defined DevEnvDir (
     (call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat") || (call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat")
 )
-tcc -run iconwriter.c icon16.png icon32.png icon.ico
+cl /nologo /O1 /w iconwriter.c
+iconwriter icon16.png icon32.png icon.ico
+del iconwriter.obj
+del iconwriter.exe
 rc res.rc
 cl /nologo /O1 /w /Gz /GS- darkpad.c /link /nodefaultlib /subsystem:windows kernel32.lib shell32.lib gdi32.lib user32.lib ole32.lib uuid.lib dwmapi.lib comdlg32.lib uxtheme.lib advapi32.lib comctl32.lib msvcrt.lib res.res
 mt.exe -manifest darkpad.exe.manifest -outputresource:darkpad.exe;1
